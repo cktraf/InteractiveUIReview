e2d6ae1872b56359f4f305201aa412b8a9e5a3ba
Carissa Traffie <carissa@traffie.com>
2025-07-24 09:07:46 -0700

    Initial Commit

---
+++ InteractiveUIReview/InteractiveUIReviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveUIReviewApp.swift
+//  InteractiveUIReview
+//
+//  Created by Scholar on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveUIReviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
