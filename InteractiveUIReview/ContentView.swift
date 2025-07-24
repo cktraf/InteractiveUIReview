6407f54691389cfa5d8c23c8954244cfc4faf139
Carissa Traffie <carissa@traffie.com>
2025-07-24 09:16:21 -0700

    added button and two images

--- InteractiveUIReview/ContentView.swift
+++ InteractiveUIReview/ContentView.swift
@@ -8,12 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var photoName = "greeceImg"
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
             Text("Hello, world!")
+            Image(photoName)
+            Button("Change image") {
+                photoName = "graceHopperImg"
+            }.font(.title2)
+                .buttonStyle(.borderedProminent)
+            .tint(.purple)
         }
         .padding()
     }
